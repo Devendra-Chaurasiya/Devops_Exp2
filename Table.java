a6c6dc45cf74e5b739b9d3f17c90e6cbad78d1e2
Devendra-Chaurasiya <devendrachaurasia1234@gmail.com>
2025-07-30 11:24:34 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,1 +1,18 @@
 
+public class Table {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please provide a number as a command-line argument.");
+            return;
+        }
+
+        try {
+            int num = Integer.parseInt(args[0]);
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number: " + args[0]);
+        }
+    }
+}
